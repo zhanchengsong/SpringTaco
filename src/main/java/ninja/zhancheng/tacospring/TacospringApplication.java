de797b34d5f319df5cf8f4cafc45f88a407b12e2
Zhancheng Song <zsong@paymentus.com>
2019-05-17 18:23:51 -0400

    Basic Spring application

---
+++ src/main/java/ninja/zhancheng/tacospring/TacospringApplication.java
@@ -0,0 +1,13 @@
+package ninja.zhancheng.tacospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ninja/zhancheng/tacospring/TacospringApplicationTests.java
@@ -0,0 +1,16 @@
+package ninja.zhancheng.tacospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacospringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
